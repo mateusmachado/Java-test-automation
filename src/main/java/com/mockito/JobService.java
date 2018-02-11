9d24496a7008bbca10f439e294f413bc0a74cd7c
mateusmachado <mmachado.csp@gmail.com>
2018-02-11 20:32:20 -0200

    implemented assignJobPosition method

--- src/main/java/com/mockito/JobService.java
+++ src/main/java/com/mockito/JobService.java
@@ -5,4 +5,14 @@ import java.util.Optional;
 public interface JobService {
 
     Optional<JobPosition> findCurrentJobPosition(Person person);
+
+    default boolean assignJobPosition(Person person, JobPosition jobPosition) {
+        if (!findCurrentJobPosition(person).isPresent()) {
+            person.setCurrentJobPosition(jobPosition);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
